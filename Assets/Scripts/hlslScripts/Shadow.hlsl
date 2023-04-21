7eaadb262ceb2103015534db54e4b460997a3a28
Xiaolong Dong <dongxiao@usc.edu>
2023-04-20 22:45:27 -0700

    added a bunch of models and textures for testing

---
+++ Assets/Scripts/hlslScripts/Shadow.hlsl
@@ -0,0 +1,23 @@
+#ifndef SHADOW_INCLUDED
+#define SHADOW_INCLUDED
+
+void MainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out float DistanceAtten, out float ShadowAtten)
+{
+#if SHADERGRAPH_PREVIEW
+    Direction = float3(0.5, 0.5, 0);
+    Color = 1;
+    DistanceAtten = 1;
+    ShadowAtten = 1;
+#else
+    float4 shadowCoord = TransformWorldToShadowCoord(WorldPos);
+    Light mainLight = GetMainLight(shadowCoord);
+    Direction = mainLight.direction;
+    Color = mainLight.color;
+    DistanceAtten = mainLight.distanceAttenuation;
+
+    ShadowAtten = MainLightRealtimeShadow(shadowCoord);
+
+#endif
+}
+
+#endif
