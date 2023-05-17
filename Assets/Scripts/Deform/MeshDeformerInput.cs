163efb8271fc229bb722758969a0b80841dda18d
Xiaolong Dong <dongxiao@usc.edu>
2023-05-16 21:56:54 -0700

    modifies deformer

--- Assets/Scripts/Deform/MeshDeformerInput.cs
+++ Assets/Scripts/Deform/MeshDeformerInput.cs
@@ -1,27 +1,34 @@
-﻿using UnityEngine;
+﻿using Unity.Burst.CompilerServices;
+using UnityEngine;
 
 public class MeshDeformerInput : MonoBehaviour {
 	
 	public float force = 10f;
 	public float forceOffset = 0.1f;
-	
-	void Update () {
-		if (Input.GetMouseButton(0)) {
+	GameObject m_player;
+	Rigidbody playerRb;
+	RaycastHit hit;
+
+    private void Start()
+    {
+		m_player = GameObject.FindGameObjectWithTag("Player");
+		playerRb = m_player.GetComponent<Rigidbody>();
+	}
+
+    void Update () {
+
+        if (Physics.Raycast(playerRb.position, Vector3.down, out hit)) {
 			HandleInput();
 		}
 	}
 
 	void HandleInput () {
-		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		
-		if (Physics.Raycast(inputRay, out hit)) {
-			MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
-			if (deformer) {
-				Vector3 point = hit.point;
-				point += hit.normal * forceOffset;
-				deformer.AddDeformingForce(point, force);
-			}
+
+		MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+		if (deformer) {
+			Vector3 point = hit.point;
+			point += hit.normal * forceOffset;
+			deformer.AddDeformingForce(point, force);
 		}
 	}
 }
