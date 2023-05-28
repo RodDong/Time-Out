887987b406b3bd34c2147757ad2acce8b494a86e
JAAACKL <lzixuan@umich.edu>
2023-05-28 15:16:05 +0800

    Level 4 Scene 5

--- Assets/Scripts/Objects/PressurePad.cs
+++ Assets/Scripts/Objects/PressurePad.cs
@@ -7,26 +7,36 @@ public class PressurePad : Triggerable
     private int count = 0;
 
     private void OnTriggerEnter(Collider other) {
-        if (count != 0 || other.tag == "Ground") { return; }
+        if (other.tag == "Ground") {
+             return; 
+        }
 
         count++;
 
-        Vector3 v = transform.position;
-        Vector3 s = transform.localScale;
-        transform.position = new Vector3(v.x, v.y - s.y / 2, v.z);
+        if (count == 1) {
+            
+            Vector3 v = transform.position;
+            Vector3 s = transform.localScale;
+            transform.position = new Vector3(v.x, v.y - s.y / 2, v.z);
 
-        GameEvents.current.TriggerEnter(id);
+            GameEvents.current.TriggerEnter(id);
+        }
+
     }
 
     private void OnTriggerExit(Collider other) {
-        if (count == 0) { return; }
 
-        triggered = false;
         count--;
-        Vector3 v = transform.position;
-        Vector3 s = transform.localScale;
-        transform.position = new Vector3(v.x, v.y + s.y / 2, v.z);
 
-        GameEvents.current.TriggerExit(id);
+        if (count == 0) {
+            
+            triggered = false;
+            
+            Vector3 v = transform.position;
+            Vector3 s = transform.localScale;
+            transform.position = new Vector3(v.x, v.y + s.y / 2, v.z);
+
+            GameEvents.current.TriggerExit(id);
+        }
     }
 }
