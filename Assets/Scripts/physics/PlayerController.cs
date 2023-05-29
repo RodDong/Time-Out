1207b252c59708df237cf257a50cfd47e90dc945
Xiaolong Dong <dongxiao@usc.edu>
2023-05-28 22:17:51 -0700

    sky box implementation and material changes

--- Assets/Scripts/physics/PlayerController.cs
+++ Assets/Scripts/physics/PlayerController.cs
@@ -69,7 +69,7 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        transform.Rotate(0, 0, 0);
+        //transform.Rotate(0, 0, 0);
 
         Debug.Log(m_Forward);
     }
@@ -130,11 +130,11 @@ public class PlayerController : MonoBehaviour
 
         moveDirection = new Vector3(moveHorizontal, 0, moveVertical);
 
-        // if (moveDirection.sqrMagnitude == 0) return;
+        if (moveDirection.sqrMagnitude == 0) return;
 
-        /*var targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+        var targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
         var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
-        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);*/
+        transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
 
         // Normalize the movement vector to ensure it's unit length
         //moveDirection = moveDirection.normalized;
@@ -146,11 +146,11 @@ public class PlayerController : MonoBehaviour
         {
             case ESlopeLevel.none:
                 // fall 
-                rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+                rb.MovePosition(rb.position + moveDirection * speed * Time.deltaTime);
                 break;
             case ESlopeLevel.ground:
                 // move along slope
-                rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+                rb.MovePosition(rb.position + moveDirection * speed * Time.deltaTime);
                 // snap to slope, leave a bit of gap
                 //rb.position = new Vector3(rb.position.x, slopeHit.point.y + playerHeight * 0.5f + 0.0f, rb.position.z);
                 //rb.MovePosition(new Vector3(rb.position.x, slopeHit.point.y + playerHeight * 0.5f + 0.1f, rb.position.z));
@@ -166,7 +166,7 @@ public class PlayerController : MonoBehaviour
             case ESlopeLevel.slope:
                 rb.useGravity = false;
                 // move along slope
-                rb.MovePosition(rb.position + GetSlopeMoveDirection() * speed * Time.fixedDeltaTime);
+                rb.MovePosition(rb.position + GetSlopeMoveDirection() * speed * Time.deltaTime);
                 // snap to slope, leave a bit of gap
                 //rb.MovePosition(new Vector3(rb.position.x, slopeHit.point.y + playerHeight * 0.5f + 0.3f, rb.position.z));
                 break;
