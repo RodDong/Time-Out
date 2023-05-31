cabe3ef957c36ba8bca26eb2705e20f627c5a8d4
Xiaolong Dong <dongxiao@usc.edu>
2023-05-30 19:09:50 -0700

    changes to models

--- Assets/Scripts/Objects/Button.cs
+++ Assets/Scripts/Objects/Button.cs
@@ -9,6 +9,8 @@ public class Button : Triggerable
     private void Update() {
         if (inRange && Input.GetKeyUp(KeyCode.E)) {
             GameEvents.current.TriggerEnter(id);
+            Vector3 buttonTopPos = gameObject.GetComponentsInChildren<Transform>()[2].position;
+            gameObject.GetComponentsInChildren<Transform>()[2].position = new Vector3(buttonTopPos.x, buttonTopPos.y - 0.1f, buttonTopPos.z) ;
         }
     }
 
