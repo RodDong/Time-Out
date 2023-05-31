03a41c3814da618695187404d2074ae6a783b0ff
JAAACKL <lzixuan@umich.edu>
2023-05-31 23:12:06 +0800

    Camera Manager

--- Assets/Scripts/Objects/Button.cs
+++ Assets/Scripts/Objects/Button.cs
@@ -17,7 +17,8 @@ public class Button : Triggerable
         if (inRange && Input.GetKeyUp(KeyCode.E) && !triggered) {
             GameEvents.current.TriggerEnter(id);
             Vector3 buttonTopPos = gameObject.GetComponentsInChildren<Transform>()[2].position;
-            gameObject.GetComponentsInChildren<Transform>()[2].position = new Vector3(buttonTopPos.x, buttonTopPos.y - 0.1f, buttonTopPos.z) ;
+            gameObject.GetComponentsInChildren<Transform>()[2].position = new Vector3(buttonTopPos.x, buttonTopPos.y - 0.1f, buttonTopPos.z);
+            triggered = true;
         }
     }
 
